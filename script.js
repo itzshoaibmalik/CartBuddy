370889ed9eafb6d416d8f215158ff093694d3c77
ShoaibCodes <malikshoaib7687@gmail.com>
2025-05-01 12:37:22 +0530

    responsive adjustments

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const summaryShippingEl = document.getElementById('summaryShipping');
 const summaryTotalEl = document.getElementById('summaryTotal');
 const loginLogoutLink = document.getElementById('loginLogoutLink'); // Get nav link
 
-// --- Sample Product Data ---
 // In a real application, this would come from an API
 const allProducts = [
     { id: 1, name: 'AeroBook Pro 15"', price: 1499.99, rentPrice: 79.99, image: 'https://images.unsplash.com/photo-1695639509828-d4260075e370?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', type: 'laptop', buy: true, rent: true, specs: { ram: '16GB', storage: '512GB SSD', cpu: 'Core i7' } },
